ff1775e1cf484fbbee2f1579fd51e39ccb22c290
Nate Jensen <najens@gmail.com>
2018-01-24 08:29:33 -0700

    docs: Create Role model

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
+# Define User model
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
@@ -10,3 +12,10 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     roles = db.relationship('Role', secondary='user_roles', # Relationships
             backref=db.backref('users', lazy='dynamic'))
+
+# Define Role model
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), server_default=u'', unique=True)
+    label = db.Column(db.Unicode(255), server_default=u'')  # for display purposes
