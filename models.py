c71e2d505609ee3ac1c820bb212849a0735d3cf6
Nate Jensen <najens@gmail.com>
2018-01-24 08:30:39 -0700

    docs: Create UserRoles model

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     public_id = db.Column(db.String(50), unique=True, nullable=False)
     username = db.Column(db.String(32), index = True)
     password_hash = db.Column(db.String(128))
-    roles = db.relationship('Role', secondary='user_roles', # Relationships
+    roles = db.relationship('Role', secondary='user_roles',
             backref=db.backref('users', lazy='dynamic'))
 
 # Define Role model
@@ -18,4 +18,11 @@ class Role(db.Model):
     __tablename__ = 'role'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), server_default=u'', unique=True)
-    label = db.Column(db.Unicode(255), server_default=u'')  # for display purposes
+    label = db.Column(db.Unicode(255), server_default=u'')
+
+# Define UserRoles model
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.id', ondelete='CASCADE'))
