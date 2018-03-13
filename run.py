bcc666a2fb38f720603a190cbfc168380c0256fe
Nate Jensen <najens@gmail.com>
2018-03-13 06:50:46 -0600

    docs: Removed auto create db from run.py

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
-from app import app, db
-from os import path
+from app import app
+
 
 if __name__ == '__main__':
-    # Create item_catalog database if it doesn't exist
-    if not path.exists('item_catalog.db'):
-        db.create_all()
     # Run app on localhost port 5000
     app.run(host='0.0.0.0', port=5000)
