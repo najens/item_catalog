51463f2ee71e586bf0aecfa0c6fce90f8144316e
Nate Jensen <najens@gmail.com>
2018-03-13 19:59:58 -0600

    style: Reorder text on run.py

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 import sys
-sys.path.insert(0, 'var/www/catalog')
 from app import app as application
+
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+sys.path.insert(0, 'var/www/catalog')
 
 
 if __name__ == '__main__':
